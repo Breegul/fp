ad8461ddbfa96153a4dc7b8980e6e7d49ca8dcca
Martin <martin.torrado.mouzo@gmail.com>
2023-01-16 16:19:00 +0000

    Add 10 results a page and forward and back buttons

--- pokemon/index.js
+++ pokemon/index.js
@@ -1,34 +1,46 @@
 function createPokemonCard(data){
-    const container = document.querySelector("#pokedex");
-    
+
     const card = document.createElement("div");
     card.className = "pokemon-card";
 
     const link = document.createElement("a");
-    link.textContent = data["name"] /*+ " #" + data["id"]*/;
-    link.href = data["url"];
+    link.textContent = data["name"]; /*+ " #" + data["id"]*/
+    link.href = `./pokemon.html?name=${data["name"]}`;
+    link.target = "blank";
     card.appendChild(link);
 
     console.log(data.url);
-    // const img = document.createElement("img");
-    // img.src = data["sprites"]["front_default"];
-    // card.appendChild(img);
+    const img = document.createElement("img");
+    let imgSrc; 
+    fetch(data.url).then(r => r.json()).then(d => imgSrc = d["sprites"]["front_default"]);
+    console.log("imgsrc " + imgSrc);
+    img.src = imgSrc
+    card.appendChild(img);
 
     container.appendChild(card);
 }
 
-// fetch("https://pokeapi.co/api/v2/pokemon/mareep")
-//     .then(res => res.json())
-//     .then((data) => {createPokemonCard(data);console.log(data);})
-//     .catch(e=>console.log("A wild pokémon appears!"))
 
 async function loadPokemon(offset) {
+    container.replaceChildren();
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`);
-    const {results} = await res.json();
-    console.log(results);
+    const { results } = await res.json();
     results.forEach(p => {
         createPokemonCard(p);
-        console.log(p);
     })
 };
-loadPokemon(0);
+
+
+document.querySelector("#next").addEventListener("click", () => {
+    offset += 10;
+    loadPokemon(offset);
+})
+
+document.querySelector("#prev").addEventListener("click", () => {
+    offset -= 10;
+    loadPokemon(offset);
+})
+
+const container = document.querySelector("#pokedex");
+let offset = 0;
+loadPokemon(offset);
