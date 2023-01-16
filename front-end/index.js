bdbf79a1d63b3c3cf46f1cc65f39c94b9677b856
Martin <martin.torrado.mouzo@gmail.com>
2023-01-16 13:43:58 +0000

    Set up and decorate cards

--- front-end/index.js
+++ front-end/index.js
@@ -1,6 +1,44 @@
 const form = document.querySelector("#selection-section > form");
+const fruitList = document.querySelector("#fruit-basket");
+
+function createDeleteButton() {
+    const button = document.createElement("button");
+    button.innerText = "Remove";
+    button.addEventListener("click", (e)=>{
+        e.target.parentElement.remove();
+    })
+    return button;
+}
+
+function createFruitCard(name){
+    const card = document.createElement("div");
+    card.className = "fruit";
+
+    const heading = document.createElement("h2");
+    heading.innerText = name;
+    card.appendChild(heading);
+
+    const img = document.createElement("img");
+    img.src = "https://placebear.com/180/180"
+    card.appendChild(img);
+
+    const p = document.createElement("p");
+    p.innerText = "my mistress eyes are nothing like the sun, \ncoral is far more red than her lips red"
+    card.appendChild(p);
+
+    const delButton = createDeleteButton();
+    card.appendChild(delButton);
+
+    return card;
+}
+
+
 
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
-    console.log(e.target["fruit-name"].value);
+    const fruit = e.target["fruit-name"].value;
+    const card = createFruitCard(fruit);
+    fruitList.appendChild(card);
+
+    e.target["fruit-name"].value = "";
 })
